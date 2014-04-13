99ec572df13a8e1bc52c5e00c15656b48ab30783
Matt Antonio <mantonio@fullsail.edu>
2014-04-13 07:57:44 -0400

    changed view file names

--- js/app/main.js
+++ js/app/main.js
@@ -3,23 +3,23 @@ var app = angular.module('mddApp', ['ngRoute']);
 app.config(function($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: 'views/list.html',
+            templateUrl: 'views/list_view.html',
             controller: 'ListController'
         })
         .when('/index', {
-            templateUrl: 'views/list.html',
+            templateUrl: 'views/list_view.html',
             controller: 'ListController'
         })
         .when('/detail/:itemId/:type', {
-            templateUrl: 'views/detail.html',
+            templateUrl: 'views/detail_view.html',
             controller: 'DetailController'
         })
         .when('/about', {
-            templateUrl: 'views/about.html',
+            templateUrl: 'views/about_view.html',
             controller: 'ListController'
         })
         .when('/portfolio', {
-            templateUrl: 'views/list.html',
+            templateUrl: 'views/list_view.html',
             controller: 'ListController'
         });
 });
